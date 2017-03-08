f5c973786158249707ca67cec056f9086ae661d9
Sergey <raven@Sergeys-MacBook-Air.local>
2017-03-09 02:18:52 +0300

    Initial Commit

---
+++ InitialViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InitialViewController.swift
+//  EXchangeApp
+//
+//  Created by Sergey on 3/9/17.
+//  Copyright © 2017 Sergey. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
